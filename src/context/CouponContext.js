205c034d8db628d60ab23157990d74867fdb0ca9
Richard Harris <richardharris916@gmail.com>
2021-04-30 07:33:25 -0500

    Added more coupons

--- src/context/CouponContext.js
+++ src/context/CouponContext.js
@@ -12,21 +12,64 @@ export const CouponProvider = ({ children }) => {
       {
         id: 1,
         title: "Back Rub",
-        text: "Good for one back rub",
+        text:
+          "Good for one back rub from your partner. NOTE: This is only redeemable for back rubs. Includes only upper/lower back and shoulders",
         redeemed: false,
       },
       {
         id: 2,
         title: "Foot Rub",
-        text: "Good for one foot rub",
+        text:
+          "Good for one foot rub from your partner. NOTE: Licking not included.",
         redeemed: false,
       },
       {
         id: 3,
         title: "Date Night",
-        text: "Good for one date night",
+        text:
+          "Good for one romantic date night. Put your heads together to decide the perfect time and place!",
         redeemed: false,
       },
+      {
+        id: 4,
+        title: "Candlelit Dinner",
+        text:
+          "Good for one romantic candlelit dinner of your choosing. WARNING: Children may or may not apply.",
+      },
+      {
+        id: 5,
+        title: "Back Rub",
+        text:
+          "Good for one back rub from your partner. NOTE: This is only redeemable for back rubs. Includes only upper/lower back and shoulders",
+        redeemed: false,
+      },
+      {
+        id: 6,
+        title: "Foot Rub",
+        text:
+          "Good for one foot rub from your partner. NOTE: Licking not included.",
+        redeemed: false,
+      }, //,
+      // {
+      //   id: 7,
+      //   title: "Candlelit Dinner",
+      //   text: "Good for one romantic candlelit dinner. WARNING: Children may or may not apply."
+      // },
+      // {
+      //   id: 8,
+      //   title: "Candlelit Dinner",
+      //   text: "Good for one romantic candlelit dinner. WARNING: Children may or may not apply."
+      // },
+      // {
+      //   id: 9,
+      //   title: "Candlelit Dinner",
+      //   text: "Good for one romantic candlelit dinner. WARNING: Children may or may not apply."
+      // },
+      // {
+      //   id: 10,
+      //   title: "Candlelit Dinner",
+      //   text: "Good for one romantic candlelit dinner. WARNING: Children may or may not apply."
+      // }
     ]);
   }
 
