8dea73643a825b269e6f2b8314ea143ea2a22c39
Richard Harris <richardharris916@gmail.com>
2021-07-04 01:59:25 -0500

    Added logic to create and set new key

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,30 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useHistory } from "react-router";
 
 import useLocalStorage from "../hooks/useLocalStorage";
-import {
-  BlockButton,
-  BlockButtonMargin,
-  WelcomeContainer,
-  WelcomeHeader,
-} from "../styles";
+import { BlockButtonMargin, WelcomeContainer, WelcomeHeader } from "../styles";
 
 const Home = () => {
   const history = useHistory();
-  const [key] = useLocalStorage("bouponKey");
+  const [key, setKey] = useLocalStorage("bouponKey");
 
   useEffect(() => {
     if (key) history.push("/coupons");
   }, [history, key]);
 
+  const createAccount = () => {
+    axios.post("/api/account/create").then((res) => {
+      setKey(res.data.key);
+    });
+  };
+
   return (
     <WelcomeContainer>
       <WelcomeHeader>Welcome!</WelcomeHeader>
-      <BlockButtonMargin primary>Get Started</BlockButtonMargin>
+      <BlockButtonMargin primary onClick={createAccount}>
+        Get Started
+      </BlockButtonMargin>
       <BlockButtonMargin secondary>I have a code</BlockButtonMargin>
     </WelcomeContainer>
   );
