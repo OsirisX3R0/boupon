d9c0c7f03cfc5a64f3fa89ec88bc696f753c0007
Richard Harris <richardharris916@gmail.com>
2021-07-08 23:56:23 -0500

    Updatec create > index Added user creation and returned key and name

--- api/account/index.js
+++ api/account/index.js
@@ -5,12 +5,20 @@ let client = new faunadb.Client({ secret: process.env.FAUNA_ADMIN_KEY });
 let q = faunadb.query;
 
 module.exports = (req, res) => {
+  let { name } = req.body;
   let key = uuid();
 
   client
     .query(q.Create(q.Collection("accounts"), { data: { key } }))
     .then(() => {
-      res.json({ key });
+      client
+        .query(q.Create(q.Collection("users"), { data: { name, key } }))
+        .then(() => {
+          res.json({ key, name });
+        })
+        .catch(({ name, message, description }) => {
+          return res.json({ name, message, description });
+        });
     })
     .catch(({ name, message, description }) => {
       return res.json({ name, message, description });
