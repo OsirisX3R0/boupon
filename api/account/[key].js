bedcd1e5882dd8128d106fccb6804ad2ff33a59a
Richard Harris <richardharris916@gmail.com>
2021-07-10 18:46:34 -0500

    Fixed get users by key

--- api/account/[key].js
+++ api/account/[key].js
@@ -3,18 +3,14 @@ const faunadb = require("faunadb");
 let client = new faunadb.Client({ secret: process.env.FAUNA_ADMIN_KEY });
 let q = faunadb.query;
 
-module.exports = async (req, res) => {
+module.exports = (req, res) => {
   const { key } = req.query;
 
-  return await client
-    .query(
-      q.Map(
-        q.Paginate(
-          q.Match(q.Index("users_by_key"), key),
-          q.Lambda("name", q.Get(q.Var("name")))
-        )
-      )
-    )
+  return client
+    .query(q.Paginate(q.Match(q.Index("users_by_key"), key)))
+    .then((response) => {
+      return res.json(response);
+    })
     .catch(({ name, message, description }) => {
       return res.json({ name, message, description });
     });
