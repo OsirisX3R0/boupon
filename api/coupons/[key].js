a5b5714ad6b0e24be247e164b7849aaa398b5e50
Richard Harris <richardharris916@gmail.com>
2021-07-16 15:44:27 -0500

    Added API route to get all account coupons

---
+++ api/coupons/[key].js
@@ -0,0 +1,23 @@
+const faunadb = require("faunadb");
+
+let client = new faunadb.Client({ secret: process.env.FAUNA_ADMIN_KEY });
+let q = faunadb.query;
+
+module.exports = (req, res) => {
+  const { key } = req.query;
+
+  client
+    .query(q.Paginate(q.Match(q.Index("coupons_by_key"), key)))
+    .then((indexResp) => {
+      Promise.all(
+        indexResp.data.map((ref) =>
+          client.query(q.Get(q.Ref(q.Collection("coupons"), ref.id)))
+        )
+      ).then((response) => {
+        return res.json(response);
+      });
+    })
+    .catch(({ name, message, description }) => {
+      return res.json({ name, message, description });
+    });
+};
