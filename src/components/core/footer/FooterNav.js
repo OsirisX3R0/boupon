049409ca205d27ab304772d94e8bf46351820dba
Richard Harris <richardharris916@gmail.com>
2021-07-21 07:37:53 -0500

    Update Home > Coupons in nav (no need for a link to Home; it's only available when logged out)

--- src/components/core/footer/FooterNav.js
+++ src/components/core/footer/FooterNav.js
@@ -2,17 +2,19 @@ import { useState, useContext } from "react";
 // import { FixedFooter } from "../../../styles";
 import { useHistory } from "react-router";
 import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
-import { Home, Person, Settings } from "@material-ui/icons";
+import { ReceiptOutlined, Person, Settings } from "@material-ui/icons";
 
 import { GlobalContext } from "../../../context/GlobalContext";
 
 const FooterNav = () => {
   const history = useHistory();
   const { key, name } = useContext(GlobalContext);
-  const [value, setValue] = useState("home");
+  const [value, setValue] = useState(
+    history.location.pathname.replace("/", "")
+  );
 
   const onChange = (event, newValue) => {
-    let route = newValue === "home" ? "/" : `/${newValue}`;
+    let route = `/${newValue}`;
     setValue(newValue);
     history.push(route);
   };
@@ -20,7 +22,11 @@ const FooterNav = () => {
   const navigation =
     key && name ? (
       <BottomNavigation value={value} onChange={onChange}>
-        <BottomNavigationAction label="Home" value="home" icon={<Home />} />
+        <BottomNavigationAction
+          label="Coupons"
+          value="coupons"
+          icon={<ReceiptOutlined />}
+        />
         <BottomNavigationAction label="Users" value="users" icon={<Person />} />
         <BottomNavigationAction
           label="Settings"
