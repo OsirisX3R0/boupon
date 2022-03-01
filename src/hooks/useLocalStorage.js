d6dc0b983da10181f3f3a2587450ce57323ad7b0
Richard Harris <richardharris916@gmail.com>
2022-02-28 18:46:32 -0600

    Updated `useLocalStorage`

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,34 +1,34 @@
 import { useRef, useState, useEffect } from "react";
 
 const useLocalStorage = (storageKey, initialValue = null) => {
-  if (typeof window !== "undefined") {
-    const key = useRef(storageKey);
-    const initial = useRef(initialValue);
-    let storageValue = localStorage.getItem(key.current)
-      ? JSON.parse(localStorage.getItem(key.current))
-      : initialValue && initialValue.current
-      ? initialValue.current
-      : initialValue;
-    const [value, setValue] = useState(storageValue);
-
-    useEffect(() => {
-      if (initial.current && localStorage.getItem(key.current) === null)
-        setStorage(initial.current);
-    }, []);
-
-    const setStorage = (newValue) => {
-      localStorage.setItem(key.current, JSON.stringify(newValue));
-      setValue(newValue);
-    };
-
-    const removeItem = () => {
-      localStorage.removeItem(key.current);
-    };
-
-    return [value, setStorage, removeItem];
-  } else {
-    return [null, null, null];
+  if (typeof window === "undefined") {
+    return initialValue;
   }
+
+  const key = useRef(storageKey);
+  const initial = useRef(initialValue);
+  let storageValue = localStorage.getItem(key.current)
+    ? JSON.parse(localStorage.getItem(key.current))
+    : initialValue && initialValue.current
+    ? initialValue.current
+    : initialValue;
+  const [value, setValue] = useState(storageValue);
+
+  useEffect(() => {
+    if (initial.current && localStorage.getItem(key.current) === null)
+      setItem(initial.current);
+  }, []);
+
+  const setItem = (newValue) => {
+    localStorage.setItem(key.current, JSON.stringify(newValue));
+    setValue(newValue);
+  };
+
+  const removeItem = () => {
+    localStorage.removeItem(key.current);
+  };
+
+  return [value, setItem, removeItem];
 };
 
 export default useLocalStorage;
